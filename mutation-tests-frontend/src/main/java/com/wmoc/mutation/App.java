8089d0b500823171b8086bbfb11344971938803d
awcasella <awcasella461@gmail.com>
2022-11-26 13:38:02 -0300

    Initial maven project

---
+++ mutation-tests-frontend/src/main/java/com/wmoc/mutation/App.java
@@ -0,0 +1,13 @@
+package com.wmoc.mutation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
